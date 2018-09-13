5418be71c4f5a2e169e5215d4b94b2bd459523c5
Nathri <nathrijacobs@gmail.com>
2018-09-13 11:56:21 +0200

    database patch on travis

--- tests/database.test.js
+++ tests/database.test.js
@@ -4,7 +4,7 @@ const pg = require("pg");
 const dbFuncs = require('../public/src/db-factory.js');;
 const Pool = pg.Pool;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/coder';
+const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/postgres';
 // 'postgresql://localhost:5432/my_products_test';
 
 const pool = new Pool({
